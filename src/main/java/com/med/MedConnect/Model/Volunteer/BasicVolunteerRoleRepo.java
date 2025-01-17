958da327cea9943e7932718ccb958d4b59d1b705
Ziad Mahmoud <ziyadmahmoud63@gmail.com>
2025-01-17 18:28:51 +0200

    Implemented decorator dp

--- src/main/java/com/med/MedConnect/Model/Volunteer/BasicVolunteerRoleRepo.java
+++ src/main/java/com/med/MedConnect/Model/Volunteer/BasicVolunteerRoleRepo.java
@@ -1,8 +1,15 @@
 package com.med.MedConnect.Model.Volunteer;
 
+import com.med.MedConnect.Model.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BasicVolunteerRoleRepo extends JpaRepository<BasicVolunteerRole, Integer> {
+    Optional<BasicVolunteerRole> findByUser(User user);
+    Optional<BasicVolunteerRole> findByUserId(int userId);
+
+
 }
